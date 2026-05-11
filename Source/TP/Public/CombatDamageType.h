bba1504381de54fd0b791e0762d09ee3aabdd595
정진우\mrjin <mrjinw@naver.com>
2026-05-11 12:50:54 +0900

    [정진우] Level
    Level Asset 추가 및 주석 수정

--- Source/TP/Public/CombatDamageType.h
+++ Source/TP/Public/CombatDamageType.h
@@ -1,14 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/DamageType.h"
 #include "CombatDamageType.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class TP_API UCombatDamageType : public UDamageType
 {
